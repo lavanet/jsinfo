6ea981d2cbadd2967f0e02831faaa649a6e01145
OneEuroQuestion <mike@oneeuroquestion.com>
2024-01-15 16:29:03 +0200

    addding the start pod scripts - something has bad spaces in the yaml

---
+++ scripts/startIndexerPod.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# jsinfo/scripts/startIndexerPod.sh
+
+trap 'echo \"Warning: Script received SIGSEGV, ignoring\"' 11;
+trap 'echo \"Error: Script terminated by signal\"; exit' 2 15;
+ulimit -c unlimited;
+while true; do
+    echo 'Starting bun src/indexer.js at:' $(date);
+    bun run src/indexer.js;
+    EXIT_CODE=$?;
+    echo 'bun run src/indexer.js exited with code:' $EXIT_CODE;
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo 'Error: bun run src/indexer.js failed';
+    fi;
+    echo 'Finished bun run src/indexer.js at:' $(date);
+    sleep 1;
+done
---
+++ scripts/startQueryPod.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# jsinfo/scripts/startQueryPod.sh
+
+# Set traps and unlimited core dump size
+trap 'echo "Warning: Script received SIGSEGV, ignoring"' 11;
+trap 'echo "Error: Script terminated by signal"; exit' 2 15;
+ulimit -c unlimited
+
+# Start the scripts in an endless loop
+run_script() {
+    command=$1
+    while true; do
+        echo "QueryPod $(date) :: Starting '$command'..."
+        eval "$command" || true
+        echo "QueryPod $(date) :: '$command' stopped, restarting..."
+        sleep 1
+    done
+}
+
+# Start the scripts in the background
+run_script "env REST_URL=http://0.0.0.0:8081 sh scripts/refreshQueryCache.sh" &
+run_script "env QUERY_PORT=8081 bun run src/query.js" &
+run_script "bun run src/query.js" &
+
+# Wait for all child processes to finish
+wait

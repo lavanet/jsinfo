a42c926c8a2422a49abaf6ff199899fa591cc152
OneEuroQuestion <mike@oneeuroquestion.com>
2024-03-01 17:00:42 +0100

    fix

--- scripts/startIndexerContainer.sh
+++ scripts/startIndexerContainer.sh
@@ -9,7 +9,7 @@ trap 'echo \"Error: Script terminated by signal\"; exit' 2 15;
 ulimit -c unlimited;
 while true; do
     echo 'Starting bun src/indexer.js in process_monitor at:' $(date);
-    python3 -u ./scripts/process_monitor.py 600 "bun run src/indexer.js"
+    python3 -u scripts/process_monitor.py 600 "bun run src/indexer.js"
     EXIT_CODE=$?;
     echo 'process_monitor.py bun run src/indexer.js exited with code:' $EXIT_CODE;
     if [ $EXIT_CODE -ne 0 ]; then

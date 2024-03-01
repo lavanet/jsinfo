602702b6a201148a187ecb3bd9c7b22a54335494
OneEuroQuestion <mike@oneeuroquestion.com>
2024-03-01 17:04:45 +0100

    small fix

--- src/query/dateUtils.ts
+++ src/query/dateUtils.ts
@@ -1,7 +1,9 @@
 // ./src/query/dateUtils.ts
 
 export function FormatDates(dataArray) {
-    if (!dataArray) return [];
+    if (!dataArray || dataArray === '' || Array.isArray(dataArray) && dataArray.length === 0 || typeof dataArray === 'object' && Object.keys(dataArray).length === 0) {
+        return [];
+    }
     return dataArray.map(item => {
         const date = new Date(item.date);
         const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];

751a231a5d722bd7cb1fb8f4d9df569a2e040ae6
OneEuroQuestion <mike@oneeuroquestion.com>
2024-07-29 10:19:03 +0200

    same supply query less sql

--- src/query/handlers/supplyHandler.ts
+++ src/query/handlers/supplyHandler.ts
@@ -1,62 +1,51 @@
 // src/query/handlers/supplyHandler.ts
 
 import { FastifyRequest, FastifyReply, RouteShorthandOptions } from 'fastify';
-import { QueryGetJsinfoReadDbInstance } from '../queryDb';
+import { QueryCheckJsinfoReadDbInstance, QueryGetJsinfoReadDbInstance } from '../queryDb';
 import * as JsinfoSchema from '../../schemas/jsinfoSchema/jsinfoSchema';
+import { eq } from 'drizzle-orm';
 
 export const SupplyRawHandlerOpts: RouteShorthandOptions = {
     schema: {
         response: {
             200: {
-                type: 'number'
+                type: 'string'
             }
         }
     }
 }
 
 export async function TotalSupplyRawHandler(request: FastifyRequest, reply: FastifyReply) {
-    const supplydb = await QueryGetJsinfoReadDbInstance().select().from(JsinfoSchema.supply);
+    await QueryCheckJsinfoReadDbInstance()
 
-    const supplyData = supplydb.reduce((acc, row) => {
-        acc.set(row.key, {
-            amount: row.amount.toString(),
-            timestamp: row.timestamp.toISOString(),
-        });
-        return acc;
-    }, new Map());
+    const result = await QueryGetJsinfoReadDbInstance()
+        .select({ amount: JsinfoSchema.supply.amount })
+        .from(JsinfoSchema.supply)
+        .where(eq(JsinfoSchema.supply.key, "total"))
 
-    const getSupplyData = (key) => {
-        const data = supplyData.get(key);
-        if (data) {
-            data.denom = "ulava";
-            return data;
-        }
-        return { amount: "-", timestamp: "-", denom: "-" };
-    };
-
-    return getSupplyData("total").amount / 1000000;
+    if (result.length > 0 && result[0].amount) {
+        const amount = Number(result[0].amount) / 1000000;
+        return amount.toString();
+    } else {
+        return "0";
+    }
 }
 
 
 export async function CirculatingSupplyRawHandler(request: FastifyRequest, reply: FastifyReply) {
+    await QueryCheckJsinfoReadDbInstance()
     const supplydb = await QueryGetJsinfoReadDbInstance().select().from(JsinfoSchema.supply);
 
-    const supplyData = supplydb.reduce((acc, row) => {
-        acc.set(row.key, {
-            amount: row.amount.toString(),
-            timestamp: row.timestamp.toISOString(),
-        });
-        return acc;
-    }, new Map());
+    const result = await QueryGetJsinfoReadDbInstance()
+        .select({ amount: JsinfoSchema.supply.amount })
+        .from(JsinfoSchema.supply)
+        .where(eq(JsinfoSchema.supply.key, "circulating"))
 
-    const getSupplyData = (key) => {
-        const data = supplyData.get(key);
-        if (data) {
-            data.denom = "ulava";
-            return data;
-        }
-        return { amount: "-", timestamp: "-", denom: "-" };
-    };
-
-    return getSupplyData("circulating").amount / 1000000;
+    if (result.length > 0 && result[0].amount) {
+        const amount = Number(result[0].amount) / 1000000;
+        return amount.toString();
+    } else {
+        return "0";
+    }
 }
+

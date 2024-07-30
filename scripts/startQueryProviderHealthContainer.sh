4e74eb440471f2c10172994c425c0c76fc7321e4
OneEuroQuestion <mike@oneeuroquestion.com>
2024-07-30 17:59:54 +0200

    remove cu limit

--- scripts/startQueryProviderHealthContainer.sh
+++ scripts/startQueryProviderHealthContainer.sh
@@ -7,7 +7,6 @@ echo "Starting query - populate container at $(date)"
 # Set traps
 trap 'echo "Warning: Script received SIGSEGV, ignoring"' 11;
 trap 'echo "Error: Script terminated by signal, ignoring"' 2 15;
-ulimit -c unlimited
 
 # Run the first script in an endless loop
 while true; do

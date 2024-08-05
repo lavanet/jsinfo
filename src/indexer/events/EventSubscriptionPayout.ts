d5041d452e59360130601e4a6d870faa381afbb5
OneEuroQuestion <mike@oneeuroquestion.com>
2024-08-05 16:00:59 +0200

    events spelling

---
+++ src/indexer/events/EventSubscriptionPayout.ts
@@ -0,0 +1,32 @@
+// from yarom 5aug:
+// {
+//     "type": "lava_subscription_payout",
+//         "attributes": [
+//             {
+//                 "key": "block",
+//                 "value": "499"
+//             },
+//             parse for valid keys like this one lava@hashhashhash (there can be many)
+//             {
+//                 "key": "cosmos1jrxcg3taxa5ln6zn0yv7x4rtaln4p3n4zrjd7p ETH1",
+//                 "value": "cu: 50 reward: 50ulava"
+//             },
+//             and this is the provider address thay payed the subscription
+//             {
+//                 "key": "cosmos1w60gjzy6l8dn0lwkdg0ffnfcv7y2htndqr4u2u ETH1",
+//                 "value": "cu: 50 reward: 50ulava"
+//             },
+//             {
+//                 "key": "subscription",
+//                 "value": "cosmos188wmcnqq0tkm7re0t9mzclv6gfk6jz35edvd6e"
+//             },
+//             {
+//                 "key": "total_cu",
+//                 "value": "100"
+//             },
+//             {
+//                 "key": "total_rewards",
+//                 "value": "100"
+//             }
+//         ]
+// }

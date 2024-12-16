882fad6d6bbc5821012274dbf44ae4781e4b0a79
OneEuroQuestion <mike@oneeuroquestion.com>
2024-12-16 10:32:35 +0100

    tlv revision

---
+++ src/utils/consts.ts
@@ -0,0 +1,6 @@
+export const REWARD_POOL_NAMES_TO_CONSIDER_IN_TOTAL_TOKEN_VALUE_CALCULATIONS = [
+    "validators_rewards_distribution_pool",
+    "validators_rewards_allocation_pool",
+    "providers_rewards_distribution_pool",
+    "providers_rewards_allocation_pool"
+];
